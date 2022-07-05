df1a052686d5ae17e65c93a93a6b4b55eb06bf88
JalilMartinez <99225250+JalilMartinez@users.noreply.github.com>
2022-07-05 14:16:12 -0500

    query selector

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// Para seleccionar elementos del html
+
+
+
+// querySelector
+const heading = document.querySelector('.header__texto h2'); //retorna 0 o 1 Elementos 
+
+heading.classList.add('Nueva-clase'); 
+heading.textContent = 'Nuevo Heading';
+console.log(heading);
+// querySelectorAll
+
+// getElementById
