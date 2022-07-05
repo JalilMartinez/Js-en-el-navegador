e5d645bc095d6a62779fab133e0e7f1651ebb5de
JalilMartinez <99225250+JalilMartinez@users.noreply.github.com>
2022-07-05 14:29:08 -0500

    query selector all

--- js/app.js
+++ js/app.js
@@ -8,6 +8,21 @@ const heading = document.querySelector('.header__texto h2'); //retorna 0 o 1 Ele
 heading.classList.add('Nueva-clase'); 
 heading.textContent = 'Nuevo Heading';
 console.log(heading);
+
 // querySelectorAll
+
+const navegacion = document.querySelectorAll('.navegacion a');
+navegacion[0].textContent = 'Nuevo';
+console.log(navegacion[0]);
+
+navegacion[0].href = 'google.com';
+
+
+// lo mismo que arriba pero sin agregar constante
+
+/* document.querySelectorAll('.navegacion a')[0].textContent = 'Nuevo';*/
+
+
+
 
 // getElementById
